1c9668ddcb8d3707c6f2b945abe67023803d67b5
Mohammed Aseem <mohammed.aseem@publicissapient.com>
2026-05-24 11:46:11 +0530

    added robots file for SEO

---
+++ go-renovate/src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+
+    sitemap: "https://go-renovate.vercel.app/sitemap.xml",
+  };
+}
