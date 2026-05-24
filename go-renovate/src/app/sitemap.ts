fbbb647de03d3cd6c1b2e1067fa1395e17d39486
Mohammed Aseem <mohammed.aseem@publicissapient.com>
2026-05-24 16:32:56 +0530

    added sitemap for SEO

--- go-renovate/src/app/sitemap.ts
+++ go-renovate/src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://go-renovate.vercel.app",
+      url: "https://gorenovate.in",
       lastModified: new Date(),
     },
   ];
